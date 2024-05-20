0d58c8f833eb677b7ae5e9de6aa37b4284fe20fc
CarlosDaCruz <carlosdavidcruz@outlook.com>
2024-05-20 15:55:28 -0500

    Ajuste Token

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -2,10 +2,10 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({    //Modo de guardar los usuarios en la base de datos
     username: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true
+        type: String,   //Tipo de dato
+        required: true, //Que sea obligatorio
+        unique: true,   //Que sea unico
+        trim: true  //Quitar espacios en blanco
     },
     email: {
         type: String,
@@ -18,7 +18,7 @@ const userSchema = new mongoose.Schema({    //Modo de guardar los usuarios en la
         required: true
     }
 }, {
-    timestamps: true
+    timestamps: true    //Para que guarde la fecha de creacion y modificacion
 });
 
 export default mongoose.model('User', userSchema);  //Modelo para crear metodos que conecten a la base de datos
