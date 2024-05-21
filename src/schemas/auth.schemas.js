e1d7506b9e66804cae5eb8f1bb75022ad163e620
CarlosDaCruz <carlosdavidcruz@outlook.com>
2024-05-20 23:35:52 -0500

    Pruebas de funcionamiento

--- src/schemas/auth.schemas.js
+++ src/schemas/auth.schemas.js
@@ -28,15 +28,9 @@ export const loginSchema = z.object({
   username: z
     .string({
       required_error: "Username is required", //Valida que Username sea un String
-    })
-    .min(4, {
-      message: "Username must be at least 4 characters long", //Valida que Username tenga al menos 4 caracteres
     }),
   password: z
     .string({
       required_error: "Password is required", //Valida que Password sea un String
-    })
-    .min(4, {
-      message: "Password must be at least 4 characters long", //Valida que Password tenga al menos 4 caracteres
     }),
 });
