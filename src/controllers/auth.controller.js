c0e00a0f329a61df22a4197744debd1f3cb31844
CarlosDaCruz <carlosdavidcruz@outlook.com>
2024-05-20 20:35:24 -0500

    Authcontroller repeticion de email

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -9,6 +9,10 @@ export const register = async (req, res) => {
 
     try {
 
+        const userFound = await User.findOne({ email}); //Busca el usuario en la base de datos
+        if (userFound) 
+            return res.status(400).json({ message: ["El email ya está en uso"] }); //Si lo encuentra, devuelve un mensaje de error
+
         const passwordHash = await bcrypt.hash(password, 10);   //Encripta la contraseña en un HASH de 10 caracteres
         const newUser = new User({
             username,
